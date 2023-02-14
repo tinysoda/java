bc5f04f3f90bdc07e9f0c64eb70023e9afe4930d
tinysoda <rainguy.shy@gmail.com>
2023-02-14 14:21:54 +0700

    added habitat

--- Cat/src/CatDeatail.java
+++ Cat/src/CatDeatail.java
@@ -3,5 +3,23 @@
  */
 public class CatDeatail {
 
-  String species
+  String species;
+  String habitat;
+
+  public String getSpecies() {
+    return this.species;
+  }
+
+  public void setSpecies(String species) {
+    this.species = species;
+  }
+
+  public String getHabitat() {
+    return this.habitat;
+  }
+
+  public void setHabitat(String habitat) {
+    this.habitat = habitat;
+  }
+
 }
